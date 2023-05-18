c38b31926caaff7a8e68747fe99140ab8ed62095
tintintintintin1 <69290507+tintintintintin1@users.noreply.github.com>
2023-05-18 12:43:47 +0800

    index

--- pages/users.js
+++ pages/users.js
@@ -32,6 +32,9 @@ function UserList({ users }) {
         <div className="fixed  flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           {users.result}
         </div>
+        <div className="fixed  flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          while this api method uses &apos;getStaticProps&apos;
+        </div>
       </div>
     </main>
   );

73c572d7d52413963ce9651f5a264fd8135327e6
tintintintintin1 <69290507+tintintintintin1@users.noreply.github.com>
2023-05-18 12:57:48 +0800

    Update users.js

--- pages/users.js
+++ pages/users.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function UserList({ users }) {
+export default function UserList() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -30,7 +30,7 @@ export default function UserList({ users }) {
           below is the fucking api response
         </div>
         <div className="fixed  flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {users.result ?? "no response"}
+          no response
         </div>
         <div className="fixed  flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           while this api method uses &apos;getStaticProps&apos;
@@ -40,13 +40,13 @@ export default function UserList({ users }) {
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${process.env.apiUrl}api/hello`);
-  const data = await res.json();
+// export async function getStaticProps() {
+//   const res = await fetch(`${process.env.apiUrl}api/hello`);
+//   const data = await res.json();
 
-  return {
-    props: {
-      users: data,
-    },
-  };
-}
+//   return {
+//     props: {
+//       users: data,
+//     },
+//   };
+// }

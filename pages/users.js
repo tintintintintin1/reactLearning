16a19e1140d72cd0b1c8509a7388c2ec9ffa52b6
tintintintintin1 <69290507+tintintintintin1@users.noreply.github.com>
2023-05-18 12:53:28 +0800

    user js

--- pages/users.js
+++ pages/users.js
@@ -30,7 +30,7 @@ export default function UserList({ users }) {
           below is the fucking api response
         </div>
         <div className="fixed  flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {users.result}
+          {users.result ?? "no response"}
         </div>
         <div className="fixed  flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           while this api method uses &apos;getStaticProps&apos;
